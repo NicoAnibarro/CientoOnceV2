8e7a653dc540329b4522f0f52be1571930aaabdf
NicoAnibarro <anibarro.nico@gmail.com>
2026-08-28 07:52:59 -0300

    Preparar Android beta 1.2.0

--- front-end/app.config.js
+++ front-end/app.config.js
@@ -2,7 +2,7 @@ module.exports = () => ({
   expo: {
     name: "Ciento Once V2",
     slug: "ciento-once-v2",
-    version: "1.1.0",
+    version: "1.2.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
